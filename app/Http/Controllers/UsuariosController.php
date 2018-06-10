82d594f57669ab5315c8202110f45cb3e7879643
thoffart <thiagohoffart321920@gmail.com>
2018-06-09 22:26:13 -0300

    banco consegue receber usuarios

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -11,6 +11,12 @@ class UsuariosController extends Controller
     {
      $usuarios = new Usuarios();
      $usuarios->nome = $request->input('nome');
+     $usuarios->email = $request->input('email');
+     $usuarios->password = $request->input('password');
+     $usuarios->nascimento = $request->input('nascimento');
+     $usuarios->boleto = $request->input('boleto');
+     $usuarios->ccredito = $request->input('ccredito');
+     $usuarios->cdebito = $request->input('cdebito');
      $usuarios->save();
      return response()->json(['usuarios' => $usuarios], 201);    
     }
